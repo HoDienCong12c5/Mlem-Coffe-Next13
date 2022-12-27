0181c637efd9098b5995a2c44afb605136098152
CongBacoor <cong@bacoor.co>
2022-12-27 22:59:20 +0700

    diencong

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.scss'
 import React from 'react'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <div>
+    header
+    </div>
+    <Component {...pageProps} />
+    <div>
+    Footer
+    </div>
+
+  </>
 }
